70fbb9f9179f3d21f9e7694d974c19b06d5377e0
Dawid Ciężarkiewicz <dawid.ciezarkiewicz@jabster.pl>
2008-05-06 23:54:33 +0200

    Use Vector, because there is no List in MIDP.

--- src/net/LocalArbiter.java
+++ src/net/LocalArbiter.java
@@ -1,14 +1,27 @@
+import java.util.Vector;
+
 class LocalArbiter implements Arbiter {
-	GameController gameController;
+	public final int BLACK_PLAYER = 0;
+	public final int WHITE_PLAYER = 1;
+	GameController playingControllers[] = new GameController[2];
+
+	Vector connectedControllers = new Vector();
 
 	public LocalArbiter() {
 	}
 
 	public void connect(GameController gc) {
-		gameController = gc;
+		connectedControllers.addElement(gc);
 	}
 
 	public void moveRequest(int x, int y) {
-		gameController.placeStone(x, y, Board.COLOR_WHITE);
+		for (int i = 0; i < connectedControllers.size(); ++i) {
+			GameController gc = (GameController)(
+				connectedControllers.elementAt(i)
+				);
+			if (gc != null) {
+				gc.placeStone(x, y, Board.COLOR_WHITE);
+			}
+		}
 	}
 }
