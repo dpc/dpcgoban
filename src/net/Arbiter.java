eaf0d9f14ce48d364d405d07e1228e61e9db5ddc
Dawid Ciężarkiewicz <dawid.ciezarkiewicz@jabster.pl>
2008-05-21 19:54:20 +0200

    Connect/disconnect facilities and possibility to choose color in Arbiter interface.

--- src/net/Arbiter.java
+++ src/net/Arbiter.java
@@ -1,4 +1,9 @@
 interface Arbiter {
-	public void moveRequest(int x, int y);
+	final static int COLOR_BLACK = 0;
+	final static int COLOR_WHITE = 1;
 	public void connect(GameController gc);
+	public void disconnect(GameController gc);
+	public void handleColor(GameController gc, int color);
+	public void unhandleColor(GameController gc, int color);
+	public void moveRequest(int x, int y);
 }
--- src/net/LocalArbiter.java
+++ src/net/LocalArbiter.java
@@ -1,19 +1,67 @@
 import java.util.Vector;
 
+/**
+ * Local interface to arbiter.
+ *
+ * This class is actual arbiter implementation.
+ */
 class LocalArbiter implements Arbiter {
-	public final int BLACK_PLAYER = 0;
-	public final int WHITE_PLAYER = 1;
-	GameController playingControllers[] = new GameController[2];
+	GameController blackController;
+	GameController whiteController;
 
 	Vector connectedControllers = new Vector();
 
 	public LocalArbiter() {
 	}
 
-	public void connect(GameController gc) {
-		connectedControllers.addElement(gc);
+	public void connect(GameController ngc) {
+
+		for (int i = 0; i < connectedControllers.size(); ++i) {
+			GameController gc = (GameController)(
+				connectedControllers.elementAt(i)
+				);
+			if (gc == null) {
+				connectedControllers.setElementAt(ngc, i);
+				return;
+			}
+		}
+
+		// no nulls, just grow
+		connectedControllers.addElement(ngc);
 	}
 
+	public void disconnect(GameController gc) {
+		for (int i = 0; i < connectedControllers.size(); ++i) {
+			if (connectedControllers.elementAt(i) == gc) {
+				connectedControllers.setElementAt(gc, i);
+			}
+		}
+	}
+	public void handleColor(GameController gc, int color) {
+		switch (color) {
+			case COLOR_WHITE:
+				whiteController= gc;
+				break;
+			case COLOR_BLACK:
+				blackController= gc;
+				break;
+		}
+	}
+
+	public void unhandleColor(GameController gc, int color) {
+		switch (color) {
+			case COLOR_WHITE:
+				if (whiteController == gc) {
+					whiteController = null;
+				}
+				break;
+			case COLOR_BLACK:
+				if (blackController == gc) {
+					blackController = null;
+				}
+				break;
+		}
+	}
 	public void moveRequest(int x, int y) {
 		for (int i = 0; i < connectedControllers.size(); ++i) {
 			GameController gc = (GameController)(
