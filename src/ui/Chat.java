04e9a9d5dd8df75b5fb1d04844706745d5b58383
Dawid Ciężarkiewicz <dawid.ciezarkiewicz@jabster.pl>
2008-05-21 21:34:26 +0200

    Appending to chat-log is now possible.

--- src/ui/Chat.java
+++ src/ui/Chat.java
@@ -1,33 +1,62 @@
 import javax.microedition.lcdui.Canvas;
+import javax.microedition.lcdui.Font;
+import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.Graphics;
 
 class Chat extends UIElementCommon {
+
+	Image chatImage = null;
+
 	public Chat(Parent parent) {
 		super(parent);
+
+		chatImage = Image.createImage(
+			parent.getXSize(),
+			parent.getYSize() - parent.getYDiv()
+			);
+
 	}
 
-	protected void repaint(Graphics g) {
-		g.setClip(
-			0, parent.getYDiv(),
-			parent.getXSize(), parent.getYSize()
+	public void appendString(String str) {
+		Font f = Font.getDefaultFont();
+		if (str.length() == 0) {
+			return;
+		}
+		for (int l = str.length(); l > 0; --l) {
+			if (f.stringWidth(str.substring(0, l)) < parent.getXSize()) {
+				drawString(str.substring(0, l));
+				appendString(str.substring(l, str.length()));
+				return;
+			}
+		}
+		drawString("??? ASSERT ERROR !!!");
+	}
+
+	public void drawString(String str) {
+		Graphics g = chatImage.getGraphics();
+		Font f = Font.getDefaultFont();
+
+		g.copyArea(
+			0, f.getHeight(),
+			chatImage.getWidth(), chatImage.getHeight() - f.getHeight(),
+			0, 0,
+			Graphics.TOP | Graphics.LEFT
 			);
 		g.setColor(0x0f0f0f0);
 		g.fillRect(
-			0, parent.getYDiv(),
-			parent.getXSize(), parent.getYSize()
+			0, chatImage.getHeight() - f.getHeight(),
+			chatImage.getWidth(), chatImage.getHeight()
 			);
 		g.setColor(0x0000040);
 		g.drawString(
-			"1, 3 - zoom in/out",
-			0, parent.getYDiv(), Graphics.TOP | Graphics.LEFT
+			str,
+			0, chatImage.getHeight(), Graphics.BOTTOM | Graphics.LEFT
 			);
-		g.drawString(
-			"arrows - move",
-			0, parent.getYDiv() + 10, Graphics.TOP | Graphics.LEFT
-			);
-		g.drawString(
-			"action - place stone",
-			0, parent.getYDiv() + 20, Graphics.TOP | Graphics.LEFT
-			);
+		markDirty();
+	}
+
+	protected void repaint(Graphics g) {
+		g.setClip(0, parent.getYDiv(), parent.getXSize(), parent.getYDiv());
+		g.drawImage(chatImage, 0, parent.getYDiv(), Graphics.TOP|Graphics.LEFT);
 	}
 }
